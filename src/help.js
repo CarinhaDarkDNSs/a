a1be58ad1c2a69635db776cb6e5d1d19623b04f5
CarinhaDarkDNSs <zenixmc.suporte@gmail.com>
2020-12-31 13:38:17 -0300

    First cmmit

---
+++ src/help.js
@@ -0,0 +1,22 @@
+const help = (prefix) => {
+	return ` *BlazeHosting - BOT* \n
+ *Comandos:* \n\n
+ comando: *${prefix}info*
+desc : veja as informações da host.\n\n
+ comando: *${prefix}discord*
+desc : pegue o link do nosso discord.\n\n
+ comando: *${prefix}marcar*
+desc : marque todos membros do grupo.\n\n
+ comandi:  *${prefix}foto*
+desc: copie a foto de um membro.\n\n
+ comando:  *${prefix}sticker*
+desc: crie figurinha com uma foto/video/gif.\n\n
+ comando:  *${prefix}banir*
+desc: banir um membro do grupo.\n\n
+ comando:  *${prefix}add*
+desc: adicionar um membro no grupo.\n\n*Site:* www blazehosting.com.br \n*Twitter:* twitter.com/blazehostingbr \n*Discord:* www.discord.gg/ATkB2ws \n\n```© BlazeHosting - Bot```.`
+}
+
+exports.help = help
+
+
